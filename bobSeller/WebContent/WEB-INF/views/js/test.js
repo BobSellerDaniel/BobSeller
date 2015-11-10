445bfe6710de937c6d74f407c2e18184b5f7b5a2
Sergio Andrés Hernández Cáceres <sergioandreshc@live.com>
2015-11-10 11:57:32 -0500

    Revert "Carga Proyecto BobSeller"
    This reverts commit 34f1f0ec97e5013cb7e02a2cd84ab61625f2ea86.

--- bobSeller/WebContent/WEB-INF/views/js/test.js
+++
@@ -1,1 +0,0 @@
-alert("imprime hola mundo");
--- bobSeller/src/core/dao/UsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package core.dao;
-
-import core.pojo.Usuario;
-
-public interface UsuarioDAO {
-	
-	public Usuario findByUsername(String usuario);
-
-}
